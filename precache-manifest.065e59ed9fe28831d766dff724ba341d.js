ce0b03706dc809b12285722bb19222e1ab6c0827
Joy Hung <yichen.hung6699@gmail.com>
2022-05-18 22:00:38 +0800

    Updates

---
+++ precache-manifest.065e59ed9fe28831d766dff724ba341d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e21c845bb4ccf7b2b1bea1030e846f9",
+    "url": "/redux-to-do-list/index.html"
+  },
+  {
+    "revision": "b1f8c82e7435c4e7481d",
+    "url": "/redux-to-do-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af8e371c80b8adf90e06",
+    "url": "/redux-to-do-list/static/js/2.31d57d17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-to-do-list/static/js/2.31d57d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f8c82e7435c4e7481d",
+    "url": "/redux-to-do-list/static/js/main.cd554b19.chunk.js"
+  },
+  {
+    "revision": "94177519310cd64f6c9d",
+    "url": "/redux-to-do-list/static/js/runtime-main.ab1671ea.js"
+  }
+]);
